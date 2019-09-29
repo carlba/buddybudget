4f172c90575266014484e98733d1615bcd7e1113
Carl Bäckström <genzorg@gmail.com>
2019-09-29 17:30:21 +0200

    Added NestJS angular integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AngularModule } from './angular/angular.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AngularModule.forRoot({
+      rootPath: 'client/dist/client'
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
