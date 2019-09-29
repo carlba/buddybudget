c051c04d8ce079fb3e4197d5a21caea47b9bd959
Carl Bäckström <genzorg@gmail.com>
2019-09-29 18:09:28 +0200

    Initial db support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AngularModule } from './angular/angular.module';
+import {
+  PostgresConnectionOptions
+} from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const typeOrmConfig = {
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+  type: 'postgres',
+  url: 'postgres://buddybudget:buddybudget@localhost:5432/buddybudget',
+} as PostgresConnectionOptions;
 
 @Module({
   imports: [
     AngularModule.forRoot({
       rootPath: 'client/dist/client'
-    })
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig)
   ],
   controllers: [AppController],
   providers: [AppService],
